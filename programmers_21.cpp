a4f9080516cd57a51e9c8d899f88aaf32ec4e385
jaemin2682 <46991162+jaemin2682@users.noreply.github.com>
2019-09-21 19:49:45 +0900

    짝수와 홀수

---
+++ programmers_21.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+	string answer = "";
+
+	if (num % 2 == 0) answer.append("Even");
+	else answer.append("Odd");
+
+	return answer;
+}
